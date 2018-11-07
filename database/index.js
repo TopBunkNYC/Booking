e8c1b27a7f4608dc929528c9d7c75adec589c4e8
Wyatt <mrwnt10@gmail.com>
2018-11-07 10:42:52 -0500

    Add CREATE and UPDATE functionality

--- database/index.js
+++ database/index.js
@@ -4,21 +4,21 @@ let stringParse = (data)=>{
   return JSON.parse(JSON.stringify(data))
 };
 
-var getData = (id)=>{
+var getListing = (id)=>{
 	console.log('Database query', id)
 
 	let aptData = {dates: [], price: 0, apartmentid: 0, minStay: 0, stars: 0, numRatings: 0, max:0};
 
 	return mysql.createConnection({
 
-		// host     : 'localhost',   Local config
-		// user     : 'root',
-		// password : '',
-		// database : 'booking'
-		host: 'booking.cksae9ebsoyz.us-east-2.rds.amazonaws.com',
-		user: 'root',
-		password: 'louisotter',
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
 		database : 'booking'
+		// host: 'booking.cksae9ebsoyz.us-east-2.rds.amazonaws.com',
+		// user: 'root',
+		// password: 'louisotter',
+		// database : 'booking'
 
 
 	}).then((conn)=>{
@@ -35,7 +35,6 @@ var getData = (id)=>{
 		return (aptDates);
 
 	}).then((raw)=>{
-
 		let data = stringParse(raw)
 		aptData.price = data[0].price;
 		aptData.apartmentid = data[0].apartmentid;
@@ -55,7 +54,50 @@ var getData = (id)=>{
 	})   
 }
 
-module.exports.getData = getData;
+var createListing = (data)=>{
+  console.log(data);
+	// let aptData = {price: 100, apartmentid: 9873888, minStay: 99, stars: 4, numRatings: 99, max:3};
+	return mysql.createConnection({
+
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'booking'
+
+	}).then((conn)=>{
+		let result = conn.query(`INSERT INTO apartment SET ?`, data);
+		conn.end();
+		return result;
+	}).catch((err)=>{
+		console.log(err);		
+	})   
+}
+
+var updateListing = (id, data) => {
+	console.log(data);
+	let aptid = 9873987
+  let aptData = {price: 222, minStay: 22, stars: 22, numRatings: 22, max:2};
+	return mysql.createConnection({
+
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'booking'
+
+	}).then((conn)=>{
+		let result = conn.query(`UPDATE apartment SET ? WHERE apartmentid = ?`, [aptData, aptid]);
+		conn.end();
+		return result;
+	}).catch((err)=>{
+		console.log(err);		
+	})  
+}
+
+module.exports = {
+	getListing,
+	createListing,
+	updateListing
+}
 
 
 
