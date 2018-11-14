211a6139f268aacbfeb0eec9ad2af4d0c8ddea79
Wyatt <mrwnt10@gmail.com>
2018-11-14 16:02:52 -0500

    Remove routes no longer needed due to using one table instead of two

--- database/models.js
+++ database/models.js
@@ -1,35 +1,25 @@
-let { db, Listing } = require('./../database/mongo.js');
+let { db, Listing } = require("./../database/mongo.js");
 
-const getListing = async (id) => {
-	return await Listing.findOne({'_id': id})
+const getListing = async id => {
+  return await Listing.findOne({ _id: id });
 };
 
-const postListing = async (listingProps) => {
-	let newListing = new Listing(listingProps);
-	return await newListing.save();
+const postListing = async listingProps => {
+  let newListing = new Listing(listingProps);
+  return await newListing.save();
 };
 
 const updateListing = async (id, listingProps) => {
-	return await Listing.findByIdAndUpdate(id, listingProps);
-}
+  return await Listing.findByIdAndUpdate(id, listingProps);
+};
 
-const deleteListing = async (id) => {
+const deleteListing = async id => {
   return await Listing.findByIdAndDelete(id);
 };
 
-const getBookedDatesForListing = (listingId) => {
-
-};
-
-const postBookedDatesForListing = (listingId) => {
-
-};
-
 module.exports = {
-	getListing,
-	postListing,
-	updateListing,
-	deleteListing,
-	getBookedDatesForListing,
-	postBookedDatesForListing
-}
+  getListing,
+  postListing,
+  updateListing,
+  deleteListing
+};
