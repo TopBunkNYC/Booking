3856770562d2938c0757485ca49bb8cd07f088cf
Wyatt <mrwnt10@gmail.com>
2018-11-29 09:21:36 -0500

    Switch to connect to deployed database instead of local

--- database/index.js
+++ database/index.js
@@ -2,7 +2,8 @@ var knex = require("knex")({
   client: "pg",
   version: "10.5",
   connection: {
-    host: "127.0.0.1",
+    host: "54.160.22.131",
+    user: "postgres",
     database: "topbunk"
   },
   pool: { min: 2, max: 10 }
