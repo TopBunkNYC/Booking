15bde200a8464ad0f332d3f34e9c4c8dce815747
Wyatt <mrwnt10@gmail.com>
2018-12-01 15:13:19 -0500

    Update styles

---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  newRelicLicenseKey: "82d6518a2388e50fe8fbb12cf28e423d2be82780"
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: DIST_DIR
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: "babel-loader",
         query: {
-          presets: ['react', 'es2015']
+          presets: ["react", "es2015"]
+        }
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: "images/[hash]-[name].[ext]"
+            }
+          }
+        ]
       }
-      },
-      {
-        test: /\.css$/,  
-        include: /node_modules/,  
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg)$/,  
-        use: [{
-            loader: 'url-loader',
-            options: { 
-                limit: 8000, // Convert images < 8kb to base64 strings
-                name: 'images/[hash]-[name].[ext]'
-            } 
-        }]
-    }
     ]
   }
 };
